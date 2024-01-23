e869a253a5e8c239e1777ca3d4df1dd5b56846c7
Aditya Singh <aditya.singh@criodo.com>
2024-01-23 22:56:13 +0530

    Ready to deploy4

---
+++ client/vite.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  
+  server:{
+    proxy:{
+      '/api':{
+        target: 'https://anytime-property.onrender.com',
+        secure: false,
+      }
+    },
+    
+  },
+  plugins: [react()],
+})
